6e7232b489fa69d2fe111a760b46f80a61d3d6bd
David Dias <daviddias.p@gmail.com>
2017-05-03 01:14:41 +0100

    all good

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-
-
-note: https://github.com/tpisto/pdf-fill-form/issues/23#issuecomment-259382139
+# gen certs
--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
-const pdfFillForm = require('pdf-fill-form')
-// const fs = require('fs')
+const fs = require('fs')
+const spawn = require('child_process').spawn
 const path = require('path')
+const each = require('async/each')
 
-const templatePath = path.join(__dirname, '../cert-20-festival.pdf')
-// const outputPath = path.join(__dirname, '../cert.pdf')
+const templatePath = path.join(__dirname, '../template.svg')
+const template = fs.readFileSync(templatePath).toString()
 
-const pdfFields = pdfFillForm.readSync(templatePath)
-console.log(pdfFields)
+const certsPath = path.join(__dirname, '../certs')
+
+const names = fs.readFileSync(path.join(__dirname, '../names.txt')).toString().split('\n').map((name) => name.trim())
+
+each(names, (name, cb) => {
+  const svgPath = certsPath + '/' + name + '.svg'
+  fs.writeFileSync(svgPath, template.replace('Participante', name))
+  convertToPDF(svgPath, name, cb)
+}, (err) => {
+  if (err) {
+    throw err
+  }
+  console.log('all done')
+})
+
+function convertToPDF (svgPath, name, callback) {
+  const certPath = 'certs/' + name + '.pdf'
+  const cp = spawn('cairosvg', [svgPath, '-o', certPath])
+
+  // cp.stdout.on('data', (data) => console.log(data.toString()))
+  // cp.stderr.on('data', (data) => console.log(data.toString()))
+
+  cp.on('close', () => callback())
+}
